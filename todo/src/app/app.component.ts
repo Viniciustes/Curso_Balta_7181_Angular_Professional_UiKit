15a513b4c67c3bd0cd852a31796fd8d3cfcd22eb
Vinícius Barreira <viniciustes@gmail.com>
2019-03-18 11:00:33 -0300

    Aula 8 - Exibindo Dados

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: any[] = [];
+  public title: String = 'Minhas Tarefas';
+
+  constructor() {
+    this.todos.push('Correr');
+    this.todos.push('Estudar');
+    this.todos.push('Cozinhar');
+  }
+
+  alteraTexto() {
+    this.title = 'Novo título';
+  }
 }
