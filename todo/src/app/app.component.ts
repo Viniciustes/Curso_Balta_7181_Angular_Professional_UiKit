8fbede333dc4f4833707555e11eb5a797ed1a1b3
Vinícius Barreira <viniciustes@gmail.com>
2019-03-18 11:16:21 -0300

    Aula 9 - Objetos Complexos

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,16 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public todos: any[] = [];
-  public title: String = 'Minhas Tarefas';
+  public todos: Todo[] = [];
+  public title: string = 'Minhas Tarefas';
 
   constructor() {
-    this.todos.push('Correr');
-    this.todos.push('Estudar');
-    this.todos.push('Cozinhar');
-  }
-
-  alteraTexto() {
-    this.title = 'Novo título';
+    this.todos.push(new Todo(1, 'Correr', false));
+    this.todos.push(new Todo(2, 'Malhar', true));
+    this.todos.push(new Todo(3, 'Trabalhar', false));
   }
 }
