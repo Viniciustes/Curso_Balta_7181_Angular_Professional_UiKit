49ade88c4ab9dd8b323ad82e69dd9f1465a7ac00
Vinícius Barreira <viniciustes@gmail.com>
2019-03-18 12:04:56 -0300

    Aula 13 - Forms Básico

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Todo } from 'src/models/todo.model';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +10,17 @@ import { Todo } from 'src/models/todo.model';
 export class AppComponent {
   public todos: Todo[] = [];
   public title = 'Minhas Tarefas';
+  public form: FormGroup;
 
-  constructor() {
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(15),
+        Validators.required
+      ])]
+    });
+
     this.todos.push(new Todo(1, 'Correr', false));
     this.todos.push(new Todo(2, 'Malhar', true));
     this.todos.push(new Todo(3, 'Trabalhar', false));
