81a900eae8f55bb0d890afc67479a5e756194dab
Vinícius Barreira <viniciustes@gmail.com>
2019-03-18 14:19:14 -0300

    Aula 18 - Lendo os Itens

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -20,15 +20,17 @@ export class AppComponent {
         Validators.required
       ])]
     });
+
+    this.load();
   }
 
   add() {
-    const title = this.form.controls['title'].value;
     const id = this.todos.length + 1;
+    const title = this.form.controls.title.value;
 
     this.todos.push(new Todo(id, title, false));
-    this.save();
     this.clear();
+    this.save();
   }
 
   clear() {
@@ -37,17 +39,29 @@ export class AppComponent {
 
   remove(todo: Todo) {
     const index = this.todos.indexOf(todo);
+
     if (index !== -1) {
       this.todos.splice(index, 1);
     }
+
+    this.save();
   }
 
   markAsDoneUndone(todo: Todo) {
     todo.done = !todo.done;
+
+    this.save();
   }
 
   save() {
     const data = JSON.stringify(this.todos);
+
     localStorage.setItem('todos', data);
+  }
+
+  load() {
+    const data = localStorage.getItem('todos');
+
+    this.todos = JSON.parse(data);
   }
 }
